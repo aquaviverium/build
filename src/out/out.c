bf8980c734974464d4110971b15fb037c0c32151
Kevin Colour <kecolour@gmail.com>
2020-07-18 00:50:36 -0700

    Playing around with SDL_SetTextureColorMod

--- src/out/out.c
+++ src/out/out.c
@@ -6,11 +6,29 @@
 /*   By: home <home@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/04 02:38:19 by home              #+#    #+#             */
-/*   Updated: 2020/07/17 23:16:22 by home             ###   ########.fr       */
+/*   Updated: 2020/07/18 00:44:45 by home             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "universe.h"
+
+/**
+* \brief Function that constructs a SDL_Rect to be used in SDL_RenderCopy(),
+* the window shall be defined as is most connivent for the graphic library,
+* with the case in SDL being that a larger `x` shall be towards the right and
+* a larger `y` shall be towards the bottom of the window.
+*
+* \param x this shall correspond to a column location on the SDL_window.
+*
+* \param y this shall correspond to a row location on the SDL_window.
+**/
+void	fill_Rect(SDL_Rect *rect, int x, int y)
+{
+	rect->h = 100;
+	rect->w = 100;
+	rect->x = x * 100;
+	rect->y = y * 100;
+}
 
 void	render_loop(void)
 {
@@ -21,6 +39,20 @@ void	render_loop(void)
 	// {
 	// 	i++;
 	// }
+
+	t_display	*display;
+	SDL_Rect dest;
+
+	fill_Rect(&dest, 5, 4);
+
+	display = get_display();
+	SDL_SetTextureColorMod(display->texture, 255, 0, 0);
+	SDL_RenderCopy(display->renderer, display->texture, NULL, &dest);
+
+	fill_Rect(&dest, 5, 5);
+
+	SDL_SetTextureColorMod(display->texture, 0, 255, 0);
+	SDL_RenderCopy(display->renderer, display->texture, NULL, &dest);
 }
 
 /**
@@ -32,10 +64,11 @@ void	render_loop(void)
 * be used as a module that can be removed, replaced, and easily
 * changed without affecting the simulation.
 *
-*/
+**/
 void	out(void)
 {
 	get_fabric();
+	render_loop();
 	SDL_RenderPresent(get_display()->renderer);
 	SDL_RenderClear(get_display()->renderer);
 
